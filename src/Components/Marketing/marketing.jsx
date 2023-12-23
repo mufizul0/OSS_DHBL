7c50ec137fea91e887d0134d74b1ca379366d4d6
Mufizul Islam <121678815+mufizul27@users.noreply.github.com>
2023-12-23 18:00:34 +0600

    first commit

---
+++ src/Components/Marketing/marketing.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const marketing = () => {
+    return (
+        <div>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+            <h1>Hello Man</h1>
+        </div>
+    );
+};
+
+export default marketing;
